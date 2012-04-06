79a75920bb4d11ff47fa51f90385990adf01d61d
Simeon F. Willbanks <sfw@simeonfosterwillbanks.com>
2012-04-06 08:51:53 -0700

    DRY requires

--- lib/nerd_quiz.rb
+++ lib/nerd_quiz.rb
@@ -1,11 +1,8 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__)) if ARGV[0] == 'dev'
 require 'yajl'
-require 'nerd_quiz/version'
-require 'nerd_quiz/color'
-require 'nerd_quiz/quiz'
-require 'nerd_quiz/question'
-require 'nerd_quiz/questions'
-require 'nerd_quiz/scorecard'
+%w{ version color quiz question questions scorecard }.each do |f|
+  require 'nerd_quiz/' + f
+end
 
 module NerdQuiz
   DEFAULT_NUMBER_OF_QUESTIONS = 10
